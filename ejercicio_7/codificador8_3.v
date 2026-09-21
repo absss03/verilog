303fe98748602dda1e7611bbf915003543a9b6bd
absss03 <barbieriabigailok@gmail.com>
2026-09-21 11:50:44 -0300

    Update: Subo ej 7 sin tb

--- ejercicio_7/codificador8_3.v
+++ ejercicio_7/codificador8_3.v
@@ -1,18 +1,18 @@
 module codificador (
-    input       [7:0]a_i,
-    output      [2:0]y_o
+    input        [7:0]a_i,
+    output   reg [2:0]y_o
 );
     
     always @(*) begin
         case (a_i)
-            8'h00 : y_o = 3'b000;
-            8'h01 : y_o = 3'b001;
-            8'h02: y_o =  3'b010;
-            8'h04 : y_o = 3'b011;
-            8'h08 : y_o = 3'b100;
-            8'h10 : y_o = 3'b101;
-            8'h20 : y_o = 3'b110;
-            8'h40 : y_o = 3'b111;
+            8'h00 : y_o =  3'b000;
+            8'h01 : y_o =  3'b001;
+            8'h02: y_o =   3'b010;
+            8'h04 : y_o =  3'b011;
+            8'h08 : y_o =  3'b100;
+            8'h10 : y_o =  3'b101;
+            8'h20 : y_o =  3'b110;
+            8'h40 : y_o =  3'b111;
             default: y_o = 3'b000;
         endcase
     end
