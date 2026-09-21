5eb12b4b65932d5803d2ff22e6736d5793fd0a7e
absss03 <barbieriabigailok@gmail.com>
2026-09-21 11:50:35 -0300

    Update: Subo ej 6 sin tb

--- ejercicio_6/decodificador2_4.v
+++ ejercicio_6/decodificador2_4.v
@@ -1,15 +1,15 @@
 module decodificador (
-    input       [1:0]a_i,
-    output      [3:0]y_o
+    input         [1:0]a_i,
+    output    reg [3:0]y_o
 );
     
     always @(*) begin
         case (a_i)
-            2'b00 : y_o = 4'b0001;
-            2'b01 : y_o = 4'b0010;
-            2'b10 : y_o = 4'b0100;
-            2'b11 : y_o = 4'b1000;
-            default: 
+            2'b00 :  y_o = 4'b0001;
+            2'b01 :  y_o = 4'b0010;
+            2'b10 :  y_o = 4'b0100;
+            2'b11 :  y_o = 4'b1000;
+            default: y_o = 4'b0000;
         endcase
     end
 endmodule
